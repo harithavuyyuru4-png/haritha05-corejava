5fd0adc728b489971ea926c5e7ef3908bf3674e7
Haritha <harithavuyyuru4@gmail.com>
2026-08-06 12:48:13 +0530

    prgm from 0 to 100 and reverse 100 to 0

---
+++ myfirstproject/src/myproject/loopstatements/Forloop.java
@@ -0,0 +1,10 @@
+package myproject.loopstatements;
+//wap to print 0 to 100 and 100 to 0 in reverse order
+public class Forloop {
+	public static void main(String[] args) {
+		System.out.println("!!!main method started");
+		for(int i=100;i>=0;i=i-1)
+		System.out.println("HELLO:"+i);
+
+}
+}
