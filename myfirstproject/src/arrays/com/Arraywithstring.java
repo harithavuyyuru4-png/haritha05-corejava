b2428320b14e1be29e9cda5b22783ba58373949d
Haritha <harithavuyyuru4@gmail.com>
2026-08-20 10:51:47 +0530

    array with string program

---
+++ myfirstproject/src/arrays/com/Arraywithstring.java
@@ -0,0 +1,22 @@
+package arrays.com;
+
+public class Arraywithstring {
+
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		String[]names=new String[6];
+		names[0]="dhoni";
+		names[1]="dhon";
+		names[2]="dhni";
+		names[3]="dhoi";
+		names[4]="honi";
+		names[5]="KLRAHUL";
+		
+		for(int i=0;i<names.length;i++) {
+			System.out.println(names[i]);
+			
+		}
+
+	}
+
+}
