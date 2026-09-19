7807b4e91514ac09fe1edfa9ad22394623f1fbb9
Haritha <harithavuyyuru4@gmail.com>
2026-09-19 22:21:22 +0530

    exception ex8

---
+++ myfirstproject/src/exceptionhandling/com/Testdemoex8.java
@@ -0,0 +1,13 @@
+package exceptionhandling.com;
+
+public class Testdemoex8 {
+
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		String str="one";
+		int x=Integer.parseInt(str);
+		System.out.println(x);
+
+	}
+
+}
